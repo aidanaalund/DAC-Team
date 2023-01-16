d82672da705dcbb844c50e2aa19b36b61d25b76e
Aidan Aalund <aidanaalund@icloud.com>
2023-01-16 12:41:01 -0600

    Made csvmaker a class that is imported into plotter for ease of use

--- Python/csvmaker.py
+++ Python/csvmaker.py
@@ -1,27 +1,28 @@
 import serial as ps
 import csv
 
-#initializes reader from the provided USB port
-arduino_port = 'COM5'
-baud = 9600
-fileName = 'test.csv'
-samples = 10
-print_labels = False
-points = []
-arduino = ps.Serial(arduino_port, baud)
-print("Connected to Arduino port: " + arduino_port)
 
-#reads the serial output
-while(samples > 0):
-    #because we're reading byte data from the serial monitor, we have to 'decode' it
-    values = arduino.readline().decode('utf-8').rstrip().split()
-
-    #points is a list of lists
-    points.append(values)
-    samples = samples-1
+def makecsv(port,file,samplescount):
+    #initializes reader from the provided USB port
+    arduino_port = port
+    baud = 9600
+    fileName = file
+    samples = samplescount
+    points = []
+    arduino = ps.Serial(arduino_port, baud)
+    print("Connected to Arduino port: " + arduino_port)
     
-with open(fileName, 'w', newline='') as csv_file:
-    writer = csv.writer(csv_file)
-    writer.writerows(points)
-
-print("Created file")
+    #reads the serial output
+    while(samples > 0):
+        #because we're reading byte data from the serial monitor, we have to 'decode' it
+        values = arduino.readline().decode('utf-8').rstrip().split()
+    
+        #points is a list of lists
+        points.append(values)
+        samples = samples-1
+        
+    with open(fileName, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerows(points)
+    
+    print("Created file")
--- Python/plotter.py
+++ Python/plotter.py
@@ -1,44 +1,50 @@
 import matplotlib.pyplot as plt
 import csv
 import numpy as np
+import csvmaker
 
-"""
-If using PuTTY to make the .csv this will read, uncheck 'Include Header', 
-change session logging to 'printable output', set the connection type to 'Serial'
-and set the name to the port Arduino is using, and make sure the file name 
-given matches with the file name used in this code (e.g temperaturedata.csv)
-"""
 
-#for parsing .csv
-x = []
-y = []
-headerCheck = 0
-plotTitle = ""
-fileName = 'test.csv'
-
-#displays data
-with open(fileName,'r') as csvfile:
-    lines = csv.reader(csvfile, delimiter=',')
-    for row in lines:
-        if headerCheck == 0:
-            plotTitle = row[0]
-            headerCheck += 1
-        elif headerCheck == 1:
-            plt.xlabel(row[0])
-            plt.ylabel(row[1])
-            headerCheck += 1
-        else:
-            x.append(int(row[0]))
-            y.append(float(row[1]))
-            
-
-x2 = np.linspace(x[0],x[-1],len(x))
-
-plt.plot(x2, y, color = 'g', linestyle = 'dashed',
-         marker = 'o',label = "Trend Line")
-
-plt.xticks(rotation = 25)
-plt.title(plotTitle, fontsize = 20)
-plt.grid()
-plt.legend()
-plt.show()
+def plot(portname,file,samples,title):
+    
+    #initialize variables
+    x = []
+    y = []
+    headerCheck = 0
+    plotTitle = title
+    fileName = file
+    port = portname
+    
+    #makes .csv
+    #input into the method is port used, file name, and amount of data points
+    csvmaker.makecsv(port,file,samples)
+           
+    #displays data
+    #TODO: add the ability to define the amount of lines we're plotting e.g. 3 different sensor values
+    with open(fileName,'r') as csvfile:
+        lines = csv.reader(csvfile, delimiter=',')
+        for row in lines:
+            if headerCheck == 0:
+                plotTitle = row[0]
+                headerCheck += 1
+            elif headerCheck == 1:
+                plt.xlabel(row[0])
+                plt.ylabel(row[1])
+                headerCheck += 1
+            else:
+                x.append(int(row[0]))
+                y.append(float(row[1]))
+                
+    
+    x2 = np.linspace(x[0],x[-1],len(x))
+    
+    plt.plot(x2, y, color = 'g', linestyle = 'dashed',
+             marker = 'o',label = "Trend Line")
+    
+    plt.xticks(rotation = 25)
+    plt.title(plotTitle, fontsize = 20)
+    plt.grid()
+    plt.legend()
+    plt.show()
+    
+if __name__ == '__main__':
+    plot('COM5','test.csv',10,'test plot')
