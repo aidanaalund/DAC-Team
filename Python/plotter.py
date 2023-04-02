60a3598ad82f93f724c5f0f23be4cb1b7e2c780a
Watts <wataru.yasueda@gmail.com>
2023-04-02 14:47:53 -0500

    File can plot 3 independent sensors

--- Python/plotter.py
+++ Python/plotter.py
@@ -1,14 +1,21 @@
+"""
+make sure pyserial is installed in your environment!
+when running, make sure to have the sensors connected 
+or comment out the makecsv line so the program will
+only rely on a premade csv.
+"""
 import matplotlib.pyplot as plt
 import csv
 import numpy as np
 import csvmaker
 
-
 def plot(portname,file,samples,title):
     
-    #initialize variables
+    #initialize variables: x-axis, y-axis data for all 3 sensors
     x = []
-    y = []
+    y1 = []
+    y2 = []
+    y3 = []
     headerCheck = 0
     plotTitle = title
     fileName = file
@@ -19,7 +26,6 @@ def plot(portname,file,samples,title):
     csvmaker.makecsv(port,file,samples)
            
     #displays data
-    #TODO: add the ability to define the amount of lines we're plotting e.g. 3 different sensor values
     with open(fileName,'r') as csvfile:
         lines = csv.reader(csvfile, delimiter=',')
         for row in lines:
@@ -32,19 +38,29 @@ def plot(portname,file,samples,title):
                 headerCheck += 1
             else:
                 x.append(int(row[0]))
-                y.append(float(row[1]))
-                
+                y1.append(float(row[1]))
+                y2.append(float(row[2]))
+                y3.append(float(row[3]))
+   #creates the linspace data type from the time values 
+    xlinspace = np.linspace(x[0],x[-1],len(x))
     
-    x2 = np.linspace(x[0],x[-1],len(x))
+    #creates 3 plots using the data and styles them
+    plt.plot(xlinspace, y1, color = 'g', linestyle = 'dashed',
+             marker = 'o',label = "Sensor 1")
+
+    plt.plot(xlinspace, y2, color = 'r', linestyle = 'dashed',
+             marker = 'o',label = "Sensor 2")
     
-    plt.plot(x2, y, color = 'g', linestyle = 'dashed',
-             marker = 'o',label = "Trend Line")
+    plt.plot(xlinspace, y3, color = 'b', linestyle = 'dashed',
+             marker = 'o',label = "Sensor 3")
     
+    #formats the graph and makes it visible
     plt.xticks(rotation = 25)
     plt.title(plotTitle, fontsize = 20)
     plt.grid()
     plt.legend()
     plt.show()
     
+#if-name-main check when running directly    
 if __name__ == '__main__':
     plot('COM5','test.csv',10,'test plot')
