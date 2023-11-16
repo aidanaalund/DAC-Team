b619d4e9451a24fd2e56f5c03e1739061735f95c
Aidan Aalund <aidanaalund@icloud.com>
2023-11-15 20:43:55 -0600

    Added some test code for the lcd

---
+++ Arduino/lcdtest/lcdtest.ino
@@ -0,0 +1,23 @@
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+//const int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
+const int rs = 48, en = 49, d4 = 50, d5 = 51, d6 = 52, d7 = 53;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  // set up the LCD's number of columns and rows:
+  lcd.begin(16, 2);
+  // Print a message to the LCD.
+  lcd.print("carbon farmer!");
+}
+
+void loop() {
+  // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  //lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  //lcd.print(millis() / 1000);
+}
